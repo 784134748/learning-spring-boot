11799ca334784cfea0d9b3aa99af6fabc558d4f6
YaLong-Lee <784134748@qq.com>
2018-08-11 00:01:46 +0800

    单元测试
    1、language level
                <plugin>
                    <groupId>org.apache.maven.plugins</groupId>
                    <artifactId>maven-compiler-plugin</artifactId>
                    <configuration>
                        <source>8</source>
                        <target>8</target>
                    </configuration>
                </plugin>
    2、durid配置项
    3、mapper单元测试

--- learning-security/src/main/java/com/yalonglee/learning/security/mapper/SysAclMapper.java
+++ learning-security/src/main/java/com/yalonglee/learning/security/mapper/SysAclMapper.java
@@ -1,8 +1,12 @@
 package com.yalonglee.learning.security.mapper;
 
+import com.yalonglee.learning.security.model.SysAcl;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 @Mapper
 public interface SysAclMapper {
+
+    SysAcl selectByPrimaryKey(@Param("id") Long id);
 
 }
---
+++ learning-test/src/test/java/com/yalonglee/learning/security/mapper/SysAclMapperTest.java
@@ -0,0 +1,24 @@
+package com.yalonglee.learning.security.mapper;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mybatis.spring.boot.test.autoconfigure.MybatisTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@MybatisTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+public class SysAclMapperTest {
+
+
+    @Autowired
+    @SuppressWarnings("all")
+    private SysAclMapper sysAclMapper;
+
+    @Test
+    public void selectSysAclByPrimaryKey(){
+        sysAclMapper.selectByPrimaryKey(1L);
+    }
+}
