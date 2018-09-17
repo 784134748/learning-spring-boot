77a3d17edab522d9b57e57794cccb4a117bfc158
YaLong_Lee <784134748@qq.com>
2018-09-17 16:07:13 +0800

    添加一些公共类/工具类

--- learning-core/src/main/java/com/yalonglee/learning/core/utils/Param2.java
+++ learning-core/src/main/java/com/yalonglee/learning/core/utils/Param2.java
@@ -16,53 +16,6 @@ public class Param2 {
     private static Logger logger = LoggerFactory.getLogger(Param2.class);
 
     /**
-     * 将javaBean转换为新增参数
-     * 非空转换
-     * @param o
-     * @return
-     */
-    public static Map<String, Object> getInsertParams(Object o) {
-        Map<String, Object> p = Maps.newHashMap();
-        //获取关联的所有类，本类以及所有父类
-        boolean ret = true;
-        Class oo = o.getClass();
-        List<Class> clazzs = new ArrayList<Class>();
-        while (ret) {
-            clazzs.add(oo);
-            oo = oo.getSuperclass();
-            if (oo == null || oo == Object.class) {
-                break;
-            }
-        }
-
-        Object value;
-        for (int i = 0; i < clazzs.size(); i++) {
-            Field[] declaredFields = clazzs.get(i).getDeclaredFields();
-            for (Field field : declaredFields) {
-                int mod = field.getModifiers();
-                //过滤 static 和 final 类型
-                if (Modifier.isStatic(mod) || Modifier.isFinal(mod)) {
-                    continue;
-                }
-                field.setAccessible(true);
-                try {
-
-                    value = field.get(o);
-                    if (value == null) {
-                        continue;
-                    }
-
-                    p.put(field.getName(), value);
-                } catch (IllegalAccessException e) {
-                    logger.error(e.getMessage());
-                }
-            }
-        }
-
-        return p;
-    }
-
-    /**
      * 将javaBean转换为更新参数
      * 全转换
      * @param o
@@ -73,7 +26,7 @@ public class Param2 {
         //获取关联的所有类，本类以及所有父类
         boolean ret = true;
         Class oo = o.getClass();
-        List<Class> clazzs = new ArrayList<Class>();
+        List<Class> clazzs = new ArrayList<>();
         while (ret) {
             clazzs.add(oo);
             oo = oo.getSuperclass();
@@ -113,7 +66,7 @@ public class Param2 {
         //获取关联的所有类，本类以及所有父类
         boolean ret = true;
         Class oo = o.getClass();
-        List<Class> clazzs = new ArrayList<Class>();
+        List<Class> clazzs = new ArrayList<>();
         while (ret) {
             clazzs.add(oo);
             oo = oo.getSuperclass();
