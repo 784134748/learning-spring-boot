e8f625632107ed39e327c17f979e45e792336379
YaLong_Lee <784134748@qq.com>
2018-09-17 16:23:45 +0800

    jar转war
    1、<packaging>war</packaging>
    2、<finalName>learning-spring-boot</finalName>
    3、
    <plugin>
                    <artifactId>maven-war-plugin</artifactId>
                    <version>2.6</version>
                    <configuration>
                        <!--如果想在没有web.xml文件的情况下构建WAR，请设置为false。-->
                        <failOnMissingWebXml>false</failOnMissingWebXml>
                    </configuration>
                </plugin>
    4、
    server:
      port: 8080
      servlet:
        context-path: /learning-spring-boot
    5、
    extends SpringBootServletInitializer

---
+++ learning-initial/src/main/java/com/yalonglee/learning/initial/InitialApplication.java
@@ -0,0 +1,18 @@
+package com.yalonglee.learning.initial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class InitialApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(LearningInitialApplication.class);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(InitialApplication.class, args);
+    }
+
+}
