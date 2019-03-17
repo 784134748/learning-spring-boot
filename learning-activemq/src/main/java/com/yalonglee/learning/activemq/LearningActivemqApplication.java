cd2f0b81549d5a3d647fd811157eeb4a36127639
YaLong-Lee <784134748@qq.com>
2019-03-17 22:38:52 +0800

    开辟消息队列模块

---
+++ learning-activemq/src/main/java/com/yalonglee/learning/activemq/LearningActivemqApplication.java
@@ -0,0 +1,13 @@
+package com.yalonglee.learning.activemq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningActivemqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearningActivemqApplication.class, args);
+    }
+
+}
